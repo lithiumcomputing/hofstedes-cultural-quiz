eeb73ab328c284babcd208834e29d55d679bf39d
Jim Li <jimli101101@gmail.com>
2019-12-01 17:51:20 -0500

    Created Quiz Component
    However, there are no forms yet.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomepageComponent},
+  {path:"quiz", component:QuizComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } export const
+RoutingComponent = [QuizComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    QuizComponent,
+    RoutingComponent
   ],
   imports: [
     BrowserModule,
