71a5fce0850501d28339b7ab21131a4e3fcf21ca
Jim Li <jimli101101@gmail.com>
2019-12-05 22:01:06 -0500

    Revamped Scoring System
    New Scoring System to accomodate a flexible, nonzero amount of
    questions. Additionally, more questions are added, and a
    percentage score is displayed in the results component.

--- src/app/quiz/quiz.questions.ts
+++ src/app/quiz/quiz.questions.ts
@@ -1,5 +1,6 @@
 export class Questions {
   static questions = [
+    // IDV and PDI Questions
     {
       'question': 'People should care for the interests of society.',
       'idv': -5,
@@ -9,9 +10,53 @@ export class Questions {
     },
     {
       'question': 'It is OK if some people have loads more money than others.',
-      'idv': -5,
+      'idv': 5,
       'pdi': 0,
       'uai': 0,
+      'mas': 0,
+    },
+    {
+      'question': 'Everyone has a place in society.',
+      'idv': 0,
+      'pdi': 5,
+      'uai': 0,
+      'mas': 0,
+    },
+    {
+      'question': 'Social influence is a significant factor in hiring decisions.',
+      'idv': 0,
+      'pdi': 5,
+      'uai': 0,
+      'mas': 0,
+    },
+    // MAS Questions
+    {
+      'question': 'Money, power, and achievement are more important than being kind and caring.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 0,
+      'mas': 5,
+    },
+    {
+      'question': 'When making society better, we must take care of animals, plants, and nature.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 0,
+      'mas': -5,
+    },
+    // UAI Questions
+    {
+      'question': 'Slow is smooth, and smooth is steady.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 5,
+      'mas': 0,
+    },
+    {
+      'question': 'Stability and security is harmful to an organization or a society.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': -5,
       'mas': 0,
     }
   ]
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Questions } from '../quiz/quiz.questions';
 
 @Component({
   selector: 'app-results',
@@ -11,27 +12,53 @@ export class ResultsComponent implements OnInit {
 
   ngOnInit() {
     var results = this.getGETParameters();
+    var totalScores = {
+      "idv": 0,
+      "pdi": 0,
+      "mas": 0,
+      "uai": 0
+    }
+    
+    // Score the Results
+    var questions = Questions.questions;
+    for (var question of questions) {
+      totalScores["idv"] += Math.abs(question["idv"]);
+      totalScores["pdi"] += Math.abs(question["pdi"]);
+      totalScores["mas"] += Math.abs(question["mas"]);
+      totalScores["uai"] += Math.abs(question["uai"]);
+    }
+    
+    var quizScores = {
+      "idv": 50+50*(results["idv"]/totalScores["idv"]),
+      "pdi": 50+50*(results["pdi"]/totalScores["pdi"]),
+      "mas": 50+50*(results["mas"]/totalScores["mas"]),
+      "uai": 50+50*(results["uai"]/totalScores["uai"])
+    }
     
     // Adjust the Graphs so that the results match.
     document.getElementById("idv-bar").innerHTML +=
       '<rect id="idv-score-bar" width="'
-    + String((results["idv"] + 100.0)/2) +
+    + String(quizScores["idv"]) +
       '%" height="100" style="fill:rgb(255,255,0);stroke-width:3;stroke:rgb(0,0,0)" />';
+    document.getElementById("idv-score").innerHTML = quizScores["idv"] + '%';
     
     document.getElementById("pdi-bar").innerHTML += 
       '<rect id="pdi-score-bar" width="'
-        + String((results["pdi"] + 100.0)/2) + 
+        + String(quizScores["pdi"]) + 
         '%" height="100" style="fill:rgb(255,0,0);stroke-width:3;stroke:rgb(0,0,0)" />';
+    document.getElementById("pdi-score").innerHTML = quizScores["pdi"] + '%';
     
     document.getElementById("mas-bar").innerHTML += 
       '<rect id="pdi-score-bar" width="'
-        + String((results["mas"] + 100.0)/2) + 
+        + String(quizScores["mas"]) + 
     '%" height="100" style="fill: rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />';
+    document.getElementById("mas-score").innerHTML = quizScores["mas"] + '%';
     
     document.getElementById("uai-bar").innerHTML += 
       '<rect id="uai-score-bar" width="'
-        + String((results["uai"] + 100.0)/2) + 
+        + String(quizScores["uai"]) + 
         '%" height="100" style="fill:rgb(128,128,128);stroke-width:3;stroke:rgb(0,0,0)" />';
+    document.getElementById("uai-score").innerHTML = quizScores["uai"] + '%';
   }
   
   getGETParameters() {
@@ -46,5 +73,4 @@ export class ResultsComponent implements OnInit {
         });
     return result;
   }
-
 }
