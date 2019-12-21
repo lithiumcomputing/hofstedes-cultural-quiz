651e275b6314fc814728fe99f900b11786cf0852
Jim Li <jimli101101@gmail.com>
2019-12-21 00:06:39 -0500

    Add More Questions
    For a grand total of 40 questions!

--- src/app/quiz/quiz.questions.ts
+++ src/app/quiz/quiz.questions.ts
@@ -54,7 +54,8 @@ export class Questions {
       'mas': 0,
     },
     {
-      'question': 'People should care for the interests of society.',
+      'question': 'People should adapt the values of their own family ' +
+                  'instead of finding their own path.',
       'idv': -5,
       'pdi': 0,
       'uai': 0,
@@ -77,7 +78,7 @@ export class Questions {
     // PDI Questions
     {
       'question': 'Everyone has a place in society.',
-      'idv': 0,
+      'idv': -5,
       'pdi': 5,
       'uai': 0,
       'mas': 0,
@@ -92,14 +93,69 @@ export class Questions {
     },
     {
       'question': 'People should care for the interests of society.',
-      'idv': -5,
-      'pdi': 0,
+      'idv': -2,
+      'pdi': 5,
+      'uai': 0,
+      'mas': -5,
+    },
+    {
+      'question': 'We may not always agree with our parents, ' +
+                  'but we should always respect them.',
+      'idv': 0,
+      'pdi': 5,
       'uai': 0,
       'mas': 0,
-    }
+    }, 
+    {
+      'question': 'We may not always agree with law enforcement officials, ' +
+                  'but we should always respect them.',
+      'idv': -5,
+      'pdi': 5,
+      'uai': 0,
+      'mas': 0,
+    }, // 5
+    {
+      'question': 'The law must be followed.',
+      'idv': -2,
+      'pdi': 5,
+      'uai': 0,
+      'mas': 2,
+    },
+    {
+      'question': 'Corruption is caused by concentrated ' +
+                  'power at the hands of the few.',
+      'idv': 0,
+      'pdi': -5,
+      'uai': 0,
+      'mas': 0,
+    },
+    {
+      'question': 'The flatter the hierarchy, the better.',
+      'idv': 0,
+      'pdi': -5,
+      'uai': 0,
+      'mas': 0,
+    },
+    {
+      'question': 'Wisdom comes from leaders with ' +
+                  'experience and high-ranking positions. ' +
+                  '(e.g. Pastors, CEOs, Queens, Presidents, Grandfathers)',
+      'idv': 0,
+      'pdi': 5,
+      'uai': 5,
+      'mas': 0,
+    },
+    {
+      'question': 'People in positions of power do not necessarily ' +
+                  'serve the interests of their group/organization/society.',
+      'idv': 0,
+      'pdi': -5,
+      'uai': 0,
+      'mas': 0,
+    }, // 10 
     // MAS Questions
     {
-      'question': 'Money, power, and achievement are more important' +
+      'question': 'Money, wealth, and achievement are more important' +
       'than being kind and caring.',
       'idv': 0,
       'pdi': 0,
@@ -107,12 +163,72 @@ export class Questions {
       'mas': 5,
     },
     {
-      'question': 'When making society better, we must take care of animals, plants, and nature.',
+      'question': 'When making society better, ' +
+                  'we must take care of animals, plants, and nature.',
       'idv': 0,
       'pdi': 0,
       'uai': 0,
       'mas': -5,
     },
+    {
+      'question': 'There should be strictly enforced, traditional gender ' +
+                  'roles in society.',
+      'idv': -5,
+      'pdi': 5,
+      'uai': 5,
+      'mas': 5,
+    },
+    {
+      'question': 'Having a kind heart is better than being cool-headed.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 0,
+      'mas': -5,
+    },
+    {
+      'question': 'Men should have a nurturing attitude.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 0,
+      'mas': -5,
+    }, // 5
+    {
+      'question': 'Men and women deserve equal pay.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 0,
+      'mas': -5,
+    },
+    {
+      'question': 'Economic growth should be one of the nation\'s ' +
+                  'highest priority.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 0,
+      'mas': 5,
+    },
+    {
+      'question': 'It is impolite to have strong egos.',
+      'idv': -5,
+      'pdi': 0,
+      'uai': 0,
+      'mas': -5,
+    },
+    {
+      'question': 'We should empower women into management.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 0,
+      'mas': -5,
+    },
+    {
+      'question': 'We should discourage women from studying STEM.' + 
+                  '(Science, Technology, Engineering, Mathematics)',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 0,
+      'mas': 5,
+    }, // 10
     // UAI Questions
     {
       'question': 'Slow is smooth, and smooth is steady.',
@@ -127,6 +243,69 @@ export class Questions {
       'pdi': 0,
       'uai': -5,
       'mas': 0,
-    }
+    },
+    {
+      'question': 'Methods that stand the test of time are generally reliable.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 5,
+      'mas': 0,
+    },
+    {
+      'question': 'The future should be seen with skepticism.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 5,
+      'mas': 0,
+    },
+    {
+      'question': 'Change should be taken in small steps at ' +
+                  'a slow pace.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 5,
+      'mas': 0,
+    }, // 5
+    {
+      'question': 'Organizations should establish more formal rules ' +
+                  'in order to have a stable and cooperative ' +
+                  'environment.',
+      'idv': 0,
+      'pdi': 5,
+      'uai': 5,
+      'mas': 0,
+    },
+    {
+      'question': 'Highly detailed plans with failsafes and contingencies ' +
+                  'hinder an organization\'s ability to function ' +
+                  'effectively.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': -5,
+      'mas': 0,
+    },
+    {
+      'question': 'Life with very little to no risk is dull and boring.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': -5,
+      'mas': 0,
+    },
+    {
+      'question': 'People who deviate from the norms significantly ' +
+                  'and negatively disturbs the surrounding social ' +
+                  'environment.',
+      'idv': 0,
+      'pdi': 0,
+      'uai': 5,
+      'mas': 0,
+    },
+    {
+      'question': 'Seeking the consensus of others is important.',
+      'idv': 0,
+      'pdi': 2,
+      'uai': 5,
+      'mas': -2,
+    } // 10
   ]
 }
